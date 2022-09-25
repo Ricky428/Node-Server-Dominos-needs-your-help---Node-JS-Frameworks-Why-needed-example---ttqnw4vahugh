7b8e0b7be2501ada8459288d3ed00ef7905dcafc
Ricky428 <rickygill6760@gmail.com>
2022-09-25 13:12:04 +0530

    done

--- src/index.js
+++ src/index.js
@@ -4,7 +4,30 @@ const httpServer = http.createServer(handleServer);
 
 
 function handleServer(req, res) {
-  
+  console.log(req.url)
+
+  if(req.url==="/welcome") {
+    res.setHeader("content-type","text/plain");
+    res.statusCode=200;
+
+    res.send("Welcome to Dominos!")
+    res.end();
+  }
+  else if(req.url==="/contact") {
+    res.setHeader("content-type","application/json")
+    res.statusCode = 200;
+    
+    let info = {
+        phone: '18602100000',
+        email: 'guestcaredominos@jublfood.com'
+    }
+    let data = JSON.stringify(info);
+    res.write(data);
+    res.end()
+  }
 }
+
+const PORT = 8081;
+httpServer.listen(PORT);
 
 module.exports = httpServer;
